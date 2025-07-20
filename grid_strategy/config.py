ed57c62763b3282bba3d015a2a09736ad1b19046
你的名稱 <Email>
2025-07-21 02:57:16 +0800

    init: 初始化網格策略架構

---
+++ grid_strategy/config.py
@@ -0,0 +1,7 @@
+# config.py
+
+API_KEY = "pcRLlaNMRU5eo6AXs5KeUAXa3BU8fimZbTkn6ByGyk4U1TvJmSiPFaV6yS2ObkI7"
+API_SECRET = "HxAkTUDoTYpDXW9s0R28h2K1F0jGBdwJtZWRm49RsYN1wyXMGhqjcYIG3VXfj9fI"
+
+BASE_URL = "https://testnet.binance.vision"  # Binance 現貨測試網
+SYMBOL = "BTCUSDT"
